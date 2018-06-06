493e89fe8aa02f3d4ec58f82c1b48fdd0347f216
Doug Waldron <guitarzan@users.noreply.github.com>
2018-06-06 16:19:29 -0400

    Add missing line breaks to Configure-Backups samples (#169)

--- iis/publish/using-web-deploy/web-deploy-automatic-backups/samples/sample4.ps1
+++ iis/publish/using-web-deploy/web-deploy-automatic-backups/samples/sample4.ps1
@@ -4,7 +4,10 @@ Configure-Backups -Enabled $true
 # Changes default backup behavior for site "foo" to enabled
 Configure-Backups -SiteName "foo" -Enabled $true
 
-# Changes the path of where backups are stored to a sibling directory named "siteName_snapshots".  # For more information about path variables, see the "backupPath" attribute in the section # "Configuring  Backup Settings on the Server for Global usage manually in IIS Config"Configure-Backups -BackupPath "{SitePathParent}\{siteName}_snapshots"
+# Changes the path of where backups are stored to a sibling directory named "siteName_snapshots".  
+# For more information about path variables, see the "backupPath" attribute in the section 
+# "Configuring  Backup Settings on the Server for Global usage manually in IIS Config"
+Configure-Backups -BackupPath "{SitePathParent}\{siteName}_snapshots"
 
 # Configures default backup limit to 5 backups
 Configure-Backups -NumberOfBackups 5
