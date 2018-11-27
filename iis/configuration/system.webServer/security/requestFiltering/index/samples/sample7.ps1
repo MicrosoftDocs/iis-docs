857daf7ca973544e0d2197af414fd54ae5250296
phbits <git@phbits.com>
2018-11-27 15:52:27 -0700

    Updated using IISCommitDelay

--- iis/configuration/system.webServer/security/requestFiltering/index/samples/sample7.ps1
+++ iis/configuration/system.webServer/security/requestFiltering/index/samples/sample7.ps1
@@ -1,8 +1,9 @@
+Start-IISCommitDelay
+
 $denyUrlSequences = Get-IISConfigSection -CommitPath 'Default Web Site' -SectionPath 'system.webServer/security/requestFiltering' | Get-IISConfigCollection -CollectionName 'denyUrlSequences'
+
 New-IISConfigCollectionElement -ConfigCollection $denyUrlSequences -ConfigAttribute @{ 'sequence' = '..' }
-
-$denyUrlSequences = Get-IISConfigSection -CommitPath 'Default Web Site' -SectionPath 'system.webServer/security/requestFiltering' | Get-IISConfigCollection -CollectionName 'denyUrlSequences'
 New-IISConfigCollectionElement -ConfigCollection $denyUrlSequences -ConfigAttribute @{ 'sequence' = ':' }
-
-$denyUrlSequences = Get-IISConfigSection -CommitPath 'Default Web Site' -SectionPath 'system.webServer/security/requestFiltering' | Get-IISConfigCollection -CollectionName 'denyUrlSequences'
 New-IISConfigCollectionElement -ConfigCollection $denyUrlSequences -ConfigAttribute @{ 'sequence' = '\' }
+
+Stop-IISCommitDelay
