a1954e7323ad0758809c1e09f1f02cdc2234571e
phbits <git@phbits.com>
2018-11-27 15:44:02 -0700

    Updated using IISCommitDelay

--- iis/configuration/system.webServer/security/requestFiltering/fileExtensions/index/samples/sample7.ps1
+++ iis/configuration/system.webServer/security/requestFiltering/fileExtensions/index/samples/sample7.ps1
@@ -1,5 +1,9 @@
+Start-IISCommitDelay
+
 $fileExtensions = Get-IISConfigSection -CommitPath 'Default Web Site' -SectionPath 'system.webServer/security/requestFiltering' | Get-IISConfigCollection -CollectionName 'fileExtensions'
+
 New-IISConfigCollectionElement -ConfigCollection $fileExtensions -ConfigAttribute @{ 'fileExtension' = '.inc'; 'allowed' = $false } -AddAt 0
 
-$fileExtensions = Get-IISConfigSection -CommitPath 'Default Web Site' -SectionPath 'system.webServer/security/requestFiltering' | Get-IISConfigElement -ChildElementName 'fileExtensions'
 Set-IISConfigAttributeValue -ConfigElement $fileExtensions -AttributeName 'applyToWebDAV' -AttributeValue $false
+
+Stop-IISCommitDelay
