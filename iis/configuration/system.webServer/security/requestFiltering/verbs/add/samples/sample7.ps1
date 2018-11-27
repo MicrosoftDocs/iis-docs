a0053ead7ea5e640214579114089739d8978eba1
phbits <git@phbits.com>
2018-11-27 16:02:23 -0700

    Updated using IISCommitDelay

--- iis/configuration/system.webServer/security/requestFiltering/verbs/add/samples/sample7.ps1
+++ iis/configuration/system.webServer/security/requestFiltering/verbs/add/samples/sample7.ps1
@@ -1,5 +1,9 @@
-$verbs = Get-IISConfigSection -CommitPath 'Default Web Site' -SectionPath 'system.webServer/security/requestFiltering' | Get-IISConfigCollection -CollectionName 'verbs'
-Set-IISConfigAttributeValue -ConfigElement $verbs -AttributeName 'applyToWebDAV' -AttributeValue $false
+Start-IISCommitDelay
 
 $verbs = Get-IISConfigSection -CommitPath 'Default Web Site' -SectionPath 'system.webServer/security/requestFiltering' | Get-IISConfigCollection -CollectionName 'verbs'
+
+Set-IISConfigAttributeValue -ConfigElement $verbs -AttributeName 'applyToWebDAV' -AttributeValue $false
+
 New-IISConfigCollectionElement -ConfigCollection $verbs -ConfigAttribute @{ 'verb'='PUT';'allowed'=$false }
+
+Stop-IISCommitDelay
