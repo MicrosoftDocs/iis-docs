f838fdf4bc241ced68c923bebe202ff105d486d1
Artur Lebedev <ras_atari@mail.ru>
2023-01-12 23:46:34 +0300

    Update sample5.vb
    Correct VBS code (current is JavaScript).

--- iis/configuration/moduleProviders/index/samples/sample5.vb
+++ iis/configuration/moduleProviders/index/samples/sample5.vb
@@ -1,12 +1,12 @@
-var adminManager = new ActiveXObject("Microsoft.ApplicationHost.WritableAdminManager"); 
-adminManager.CommitPath = "MACHINE/WEBROOT"; 
-adminManager.SetMetadata("pathMapper", "AdministrationConfig");
-var moduleProvidersSection = adminManager.GetAdminSection("moduleProviders", "MACHINE/WEBROOT"); 
+Set adminManager = WScript.CreateObject("Microsoft.ApplicationHost.WritableAdminManager")
+adminManager.CommitPath = "MACHINE/WEBROOT"
+adminManager.SetMetadata "pathMapper", "AdministrationConfig"
+Set modulesSection = adminManager.GetAdminSection("modules", "MACHINE/WEBROOT")
 
-var moduleProvidersCollection = moduleProvidersSection.Collection;
-var addElement = moduleProvidersCollection.CreateNewElement("add");
-addElement.Properties.Item("name").Value = "ContosoProvider";
-addElement.Properties.Item("type").Value = "Contoso.Provider, System.Web, Version=1.0.0.0, Culture=neutral, PublicKeyToken=426f62526f636b73";
-moduleProvidersCollection.AddElement(addElement);
+Set moduleProvidersCollection = moduleProvidersCollection.Collection
+Set addElement = moduleProvidersCollection.CreateNewElement("add")
+addElement.Properties.Item("name").Value = "ContosoProvider"
+addElement.Properties.Item("type").Value = "Contoso.Provider, System.Web, Version=1.0.0.0, Culture=neutral, PublicKeyToken=426f62526f636b73"
+moduleProvidersCollection.AddElement(addElement)
 
-adminManager.CommitChanges();
+adminManager.CommitChanges()
