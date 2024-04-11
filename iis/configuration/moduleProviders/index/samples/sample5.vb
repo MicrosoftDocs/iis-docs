aaceff5731a5a153a05ecd8913880d776a52cf1c
Wade Pickett <wpickett@microsoft.com>
2024-04-11 11:38:18 -0700

    Apply suggestions from code review
    Fixed my error, ensured `modulesSection` is used throughout.

--- iis/configuration/moduleProviders/index/samples/sample5.vb
+++ iis/configuration/moduleProviders/index/samples/sample5.vb
@@ -1,9 +1,9 @@
 Set adminManager = WScript.CreateObject("Microsoft.ApplicationHost.WritableAdminManager")
 adminManager.CommitPath = "MACHINE/WEBROOT"
 adminManager.SetMetadata "pathMapper", "AdministrationConfig"
-Set moduleProvidersCollection = adminManager.GetAdminSection("modules", "MACHINE/WEBROOT")
+Set modulesSection = adminManager.GetAdminSection("modules", "MACHINE/WEBROOT")
 
-Set moduleProvidersCollection = moduleProvidersCollection.Collection
+Set moduleProvidersCollection = modulesSection.Collection
 Set addElement = moduleProvidersCollection.CreateNewElement("add")
 addElement.Properties.Item("name").Value = "ContosoProvider"
 addElement.Properties.Item("type").Value = "Contoso.Provider, System.Web, Version=1.0.0.0, Culture=neutral, PublicKeyToken=426f62526f636b73"
