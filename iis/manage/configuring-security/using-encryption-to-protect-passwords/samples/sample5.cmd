e1ccd2e05f2af344a988a30769b8e71ee7e6c0fb
Lex Li <425130+lextm@users.noreply.github.com>
2025-03-31 16:04:08 -0400

    Update sample5.cmd to match test results (#1091)

--- iis/manage/configuring-security/using-encryption-to-protect-passwords/samples/sample5.cmd
+++ iis/manage/configuring-security/using-encryption-to-protect-passwords/samples/sample5.cmd
@@ -1,13 +1,9 @@
-keyContainerName="NetFrameworkConfigurationKey" cspProviderName=""
-useMachineContainer="true" useOAEP="false" name="RsaProtectedConfigurationProvider"
-type="System.Configuration.RsaProtectedConfigurationProvider,System.Configuration,
+<configProtectedData defaultProvider="RsaProtectedConfigurationProvider">
+    <providers>
+        <add name="RsaProtectedConfigurationProvider" type="System.Configuration.RsaProtectedConfigurationProvider,System.Configuration, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a" description="Uses RsaCryptoServiceProvider to encrypt and decrypt" keyContainerName="NetFrameworkConfigurationKey" cspProviderName="" useMachineContainer="true" useOAEP="false"/>
 
-Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a" />
-useMachineProtection="true" keyEntropy="" name="DataProtectionConfigurationProvider"
-type="System.Configuration.DpapiProtectedConfigurationProvider,System.Configuration,
-Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a" />
+        <add name="DataProtectionConfigurationProvider" type="System.Configuration.DpapiProtectedConfigurationProvider,System.Configuration, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a" description="Uses CryptProtectData and CryptUnProtectData Windows APIs to encrypt and decrypt" useMachineProtection="true" keyEntropy=""/>
 
-cspProviderName="" useMachineContainer="true" useOAEP="false"
-name="Rsa_WAS"
-type="System.Configuration.RsaProtectedConfigurationProvider,System.Configuration,
-Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a" />
+        <add name="Rsa_WAS" type="System.Configuration.RsaProtectedConfigurationProvider,System.Configuration, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a" description="RsaKeyForWAS" keyContainerName="iisWasKey" cspProviderName="" useMachineContainer="true" useOAEP="false" />
+    </providers>
+</configProtectedData>
